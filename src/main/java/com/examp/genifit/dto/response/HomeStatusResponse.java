41d07318ea9143e825a7d397ffafbd58e387e375
Mai Le Nguyen <mailenguyen2301@gmail.com>
2026-06-06 22:26:15 +0700

    [FEAT] Home Status & Charts

--- src/main/java/com/examp/genifit/dto/response/HomeStatusResponse.java
+++ src/main/java/com/examp/genifit/dto/response/HomeStatusResponse.java
@@ -1,20 +1,12 @@
 package com.examp.genifit.dto.response;
 
 import com.examp.genifit.entity.StatusColor;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class DailySummaryResponse {
-
-    private LocalDate date;
+public class HomeStatusResponse {
 
     private Double totalCalories;
 
--- src/main/java/com/examp/genifit/repository/DailyLogRepository.java
+++ src/main/java/com/examp/genifit/repository/DailyLogRepository.java
@@ -1,5 +1,6 @@
 package com.examp.genifit.repository;
 
+import com.examp.genifit.dto.response.HomeStatusResponse;
 import com.examp.genifit.entity.DailyLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,5 +11,5 @@ import java.util.Optional;
 public interface DailyLogRepository extends JpaRepository<DailyLog, Integer> {
     Optional<DailyLog> findByUser_UserIdAndLogDate(Integer userId, LocalDate logDate);
     Optional<DailyLog> findByGuest_GuestIdAndLogDate(Integer guestId, LocalDate logDate);
-    List<DailyLog> findAllByUser_UserIdAndLogDateBetween(Integer userId, LocalDate startDate, LocalDate endDate);
+    List<DailyLog> findAllByUser_UserIdAndLogDateBetweenOrderByLogDateAsc(Integer userId, LocalDate startDate, LocalDate endDate);
 }
